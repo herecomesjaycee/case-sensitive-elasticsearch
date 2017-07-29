791693e62587474158ffc73c5796f0b22ac72af5
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-07-29 22:38:24 +0100

    To record case-sensitive elasticsearch settings

---
+++ app.rb
@@ -0,0 +1,50 @@
+require 'elasticsearch'
+
+client = Elasticsearch::Client.new log: true
+
+TERMS = ["Dioxide", "dioxide", "dioxide"]
+
+client.indices.create index: 'products',
+                      body: {
+                        settings: {
+                          # index: {
+                          #   number_of_shards: 1,
+                          #   number_of_replicas: 0,
+                          #   'routing.allocation.include.name' => 'node-1'
+                          # },
+                          analysis: {
+                            # filter: {
+                            #   ngram: {
+                            #     type: 'nGram',
+                            #     min_gram: 3,
+                            #     max_gram: 25
+                            #   }
+                            # },
+                            analyzer: {
+                              case_sensitive: {
+                                tokenizer: 'keyword',
+                                type: 'custom'
+                              },
+                              # ngram_search: {
+                              #   tokenizer: 'whitespace',
+                              #   filter: ['lowercase', 'stop'],
+                              #   type: 'custom'
+                              # }
+                            }
+                          }
+                        },
+                        mappings: {
+                          document: {
+                            properties: {
+                              title: {
+                                type: 'string',
+                                analyzer: 'case_sensitive'
+                              }
+                            }
+                          }
+                        }
+                      }
+
+TERMS.each_with_index do |term, index|
+  client.index  index: 'products', type: 'directory', id: index + 1, body: { title: "#{term}" }
+end
